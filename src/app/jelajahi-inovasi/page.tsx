ea9157d35c16d2fc5dac8a64c77d9843f869a1b4
Aoddi <Aoddi@example.com>
2025-10-23 10:52:40 +0700

    feat: menambahkan card untuk halaman jelajahi-inovasi

--- src/app/jelajahi-inovasi/page.tsx
+++ src/app/jelajahi-inovasi/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+
 import InovasiCard from '@/components/ui/fragments/custom-ui/card/InovasiCard'
+import { Input } from '@/components/ui/fragments/shadcn-ui/input'
 import React from 'react'
 
 function page() {
   return (
+    <>
+
     <InovasiCard />
+    <Input placeholder="Ini adalah input dari shadcn-ui" />
+    </>
+
   )
 }
 
